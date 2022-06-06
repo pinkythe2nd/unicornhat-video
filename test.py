266e96560a46dc3c60ceeab6d39716a250b02f02
pinkythe2nd <69367999+pinkythe2nd@users.noreply.github.com>
2022-06-06 23:13:08 +0100

    Update test.py

--- test.py
+++ test.py
@@ -17,22 +17,21 @@ def play(data, duration):
 def main(filename):
     pg.mixer.init()
     array = load(filename)
-    duration = array[1]
-    frames = len(array[2])
+    duration = array.pop(1)
+    x = threading.Thread(target=play, args=(array.pop(0), duration, ), daemon=True)
+    frames = len(array)
     time_for_each_frame = duration / frames
-    x = threading.Thread(target=play, args=(array[0], duration, ))
     x.start()
-    for x in array[2]:
+    for frame in array:
         start = time.time()
-        for i in range(16):
-            for t in range(16):
-                unicornhathd.set_pixel(i, t, x[t][i][0], x[t][i][1], x[t][i][2])
+        for x in range(16):
+            for y in range(16):
+                unicornhathd.set_pixel(x, y, frame[x][y][0], x[x][y][1], x[x][y][2])
         unicornhathd.show()
         end = time.time()
         time_taken = end - start
         if time_taken < time_for_each_frame:
             time.sleep(time_for_each_frame - time_taken)
-
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description= "input a filename of a video and returns a serialized 2D array of 16 * 16 numbers of each frame of the video")
